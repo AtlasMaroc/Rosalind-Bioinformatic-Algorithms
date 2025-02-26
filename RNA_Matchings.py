6be93594a2e8aa43e6b1953768bba0088bc949d8
AtlasMaroc <moha.castellajo2002@gmail.com>
2025-02-26 19:51:47 +0000

    added some goals i need to look up the next session

--- RNA_Matchings.py
+++ RNA_Matchings.py
@@ -1,4 +1,17 @@
+"""
+this algorithm is used to find the perfect matching for a given RNA string
+"""
+
+#something's I need to look up
+#1: how to implement a graph in python
+#2: how to design a recursive function
+#3: perfect matching in a complete graph
+#4: bonding graph, adjacency list
+#5: define the variables I will be working with such as Kn and Pn
+#6: undirected cyclic graph
+
 class Graph:
+    #object node with the label and the corresponding edges
     class node(object):
           def __init__(self, label):
               self.lab = label
