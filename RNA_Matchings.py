d27eeac8c18ca5262caa6391cc905ccae06a6302
AtlasMaroc <moha.castellajo2002@gmail.com>
2025-02-26 19:39:46 +0000

    completed the challenge

---
+++ RNA_Matchings.py
@@ -0,0 +1,7 @@
+class Graph:
+    class node(object):
+          def __init__(self, label):
+              self.lab = label
+              self.adjacent = {}
+              self.basepair = {}
+    def __init__(self):
